71085de3c04386e69bb24f608631ec1ede0823a6
Bionic9 <bionic88811@email.com>
2025-05-26 02:28:19 +0500

    первая сборка iiko_order_site_v2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React, { useState, useEffect } from "react";
+import QRCode from "qrcode";
+import './App.css';
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Вторая версия сайта заказа — скоро здесь всё взлетит</h1>
+    </div>
+  );
+}
